1d563348c0e342ea02ef27f8ebb9c735275c407c
nmoor004 <38025224+nmoor004@users.noreply.github.com>
2019-11-25 10:52:57 -0800

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -1,22 +1,108 @@
-/*	Author: nmoor004
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
+
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+////////////////////////////////////////////////////
+////////////////TIMER FUNCTIONS
+//////////////////////////////////////////////////
+
+volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C Programmer should clear to 0.
+unsigned long _avr_timer_M = 1;	       // Start count from here, down to 0. Default 1 ms.
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+unsigned short count = 0x00;
+void TimerOn() {
+	// AVR timer/counter controller register TCCR1
+	TCCR1B = 0x0B;     // bit3 = 0: CTC mode (clear timer on compare)
+		          // bit2bit1bit0 = 011: pre-scaler /64
+			 // 00001011: 0x0B
+			// SO, 8 MHz clock or 8,000,000 / 64 = 125,000 ticks/s
+		       // Thus, TCNT1 register will count at 125,000 tick/s
+	// AVR output compare register OCR1A.
+	OCR1A = 125;    // Timer interrupt will be generated when TCNT1==OCR1A
+		       // We want a 1 ms tick. 0.001s * 125,000 ticks/s = 125
+		      // So when TCNT1 register equals 125,
+		     // 1 ms has passed. Thus, we compare to 125.
+	// AVR timer interrupt mask register
+	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+	// Initialize avr counter
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	// TimerISR will be called every _avr_timer_cntcurr milliseconds
+	// Enable globla interrupts
+	SREG |= 0x80; // 0x80: 1000000
+}
+void TimerOff() {
+	TCCR1B = 0x00; /// bit3bit1bit0 = 000: timer off
+}
+void TimerISR() {
+	TimerFlag = 1;
+}
+// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		TimerISR(); 	      // Call the ISR that the user uses
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+
+
+
+///////////RIGHT INPUTS w/ luckylight label facing towards top of board portb
+//BIT 0 = ROW 1
+//BIT 1 = ROW 2
+//BIT 2 = COL 7
+//BIT 3 = ROW 8
+//BIT 4 = COL 5
+//BIT 5 = ROW 3
+//BIT 6 = COL 8
+//BIT 7 = ROW 5
+
+
+////////////LEFT INPUTS portd
+//BIT 0 = COL 2
+//BIT 1 = ROW 7
+//BIT 2 = ROW 6
+//BIT 3 = COL 1
+//BIT 4 = ROW 4
+//BIT 5 = COL 3
+//BIT 6 = COL 6
+//BIT 7 =  COL 4
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+	DDRB = 0xFF; PORTB = 0x00; //OUTPUT
+	DDRD = 0xFF; PORTD = 0x00; //OUTPUT
+	DDRC = 0xFF; PORTC = 0x00;
+	TimerSet(1000);
+	TimerOn();
     /* Insert your solution below */
-    while (1) {
+	PORTC = 0x01;
+	PORTB = 0x01;
+    while (1) {	
+	PORTC = PORTC << 1;
+	PORTB = PORTB << 1;
+	if (PORTC == 0x00) {
+		PORTC = 0x01;
+	}
+	if (PORTB == 0x00) {
+		PORTB = 0x01;
+	}
+
+	while (!TimerFlag);
+	TimerFlag = 0;
+
+	
 
     }
     return 1;
