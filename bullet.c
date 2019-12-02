aa4900ea428025c63600965a9b66c840cf27719f
nmoor004 <nmoor004@ucr.edu>
2019-12-02 09:31:17 -0800

    Creating bullet logic

---
+++ bullet.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+
+
+struct bullet {
+	char x; //X & Y start from 0 for array access
+	char y;
+	char start;
+	char direction; // 0 up, 1 down, 2 left, 3 right
+
+	
+};
+
+bullet A;
+bullet B;
+
+bullet bulletArray[2] = {A, B};
+
+
+int main(void) {
+	
+	
+	
+	while(1)
+			
+
+	
+	
+	
+	return 1;
+}
