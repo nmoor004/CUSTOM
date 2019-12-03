89ffc2cbc0a18d99b5e612c1b8e18d1775a50288
nmoor004 <nmoor004@ucr.edu>
2019-12-02 19:15:41 -0800

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,141 +1,3 @@
-
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
-
-
-  /////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////////////////-----------TIMER-----------//////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-
-volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C Programmer should clear to 0.
-unsigned long _avr_timer_M = 1;	       // Start count from here, down to 0. Default 1 ms.
-unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
-unsigned short count = 0x00;
-void TimerOn() {
-	// AVR timer/counter controller register TCCR1
-	TCCR1B = 0x0B;     // bit3 = 0: CTC mode (clear timer on compare)
-		          // bit2bit1bit0 = 011: pre-scaler /64
-			 // 00001011: 0x0B
-			// SO, 8 MHz clock or 8,000,000 / 64 = 125,000 ticks/s
-		       // Thus, TCNT1 register will count at 125,000 tick/s
-	// AVR output compare register OCR1A.
-	OCR1A = 125;    // Timer interrupt will be generated when TCNT1==OCR1A
-		       // We want a 1 ms tick. 0.001s * 125,000 ticks/s = 125
-		      // So when TCNT1 register equals 125,
-		     // 1 ms has passed. Thus, we compare to 125.
-	// AVR timer interrupt mask register
-	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
-	// Initialize avr counter
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	// TimerISR will be called every _avr_timer_cntcurr milliseconds
-	// Enable globla interrupts
-	SREG |= 0x80; // 0x80: 1000000
-}
-void TimerOff() {
-	TCCR1B = 0x00; /// bit3bit1bit0 = 000: timer off
-}
-void TimerISR() {
-	TimerFlag = 1;
-}
-// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
-ISR(TIMER1_COMPA_vect) {
-	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
-	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
-	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
-		TimerISR(); 	      // Call the ISR that the user uses
-		_avr_timer_cntcurr = _avr_timer_M;
-	}
-}
-// Set TimerISR() to tick every M ms
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /////////////////////////////////////////////////////////////////////////////////////////
- //////////////////////////////-----------JOYSTICK-----------/////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-
-
-uint16_t x, y;
-unsigned char xData, yData;
-
-
-
-void InitADC(void)
-{
-	ADMUX|=(1<<REFS0);
-	ADCSRA|=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); //ENABLE ADC, PRESCALER 128
-}
-
-uint16_t readadc(uint8_t ch)
-{
-	ch&=0b00000111;         //ANDing to limit input to 7
-	ADMUX = (ADMUX & 0xf8)|ch;  //Clear last 3 bits of ADMUX, OR with ch
-	ADCSRA|=(1<<ADSC);        //START CONVERSION
-	while((ADCSRA)&(1<<ADSC));    //WAIT UNTIL CONVERSION IS COMPLETE
-	return(ADC);        //RETURN ADC VALUE
-}
-
-
-  ///////////////////////////////////////////////////////////////////////////////////////////
-  /////////////////////////////----------BULLET LOGIC---------//////////////////////////////
-  /////////////////////////////////////////////////////////////////////////////////////////
-struct bullet {
-	char x;
-	char y;
-	char start;
-	char direction;
-	
-	};
-
-bullet bulletArray[5] = {A, B, C, D, E}
-
-  /////////////////////////////////////////////////////////////////////////////////////////
- //////////////////////////////----------GAME LOGIC---------//////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-
-int GameBoard[8][8] = {0};
-
-enum Game_States {} game_state;
-
-void gameTick() {
-	
-	
-}
-
-
-
-
-
-  /////////////////////////////////////////////////////////////////////////////////////////
- ////////////////////////////----------BOARD RENDERING---------///////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////
-
-enum Render_States {} render_state;
-
-
-void renderTick() {
-	
-	
-}
-
 ///////////////MATRIX SCHEMATICS
 
 
@@ -182,33 +44,380 @@ void renderTick() {
    BIT 4 = COL 5 (old bBIT 4)
    BIT 5 = COL 6 (old dBIT 6)
    BIT 6 = COL 7 (old bBIT 2)
-   BIT 7 = COL 8 (old bBIT 6)
+   BIT 7 = COL 8 (old bBIT 6) */
+   
+   
+   #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 
-*/ 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-	DDRB = 0xFF; PORTB = 0x00; //OUTPUT
-	DDRD = 0xFF; PORTD = 0x00; //OUTPUT
-	DDRC = 0xFF; PORTC = 0x00;
-	DDRA = 0x00; PORTA = 0xFF;
-	TimerSet(1000);
-	TimerOn();
 
 
-    while (1) {	
 
 
-	while (!TimerFlag);
-	TimerFlag = 0;
 
-	
+  //////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////---------TIMER & ADC-------//////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
 
-    }
-    return 1;
+  volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C Programmer should clear to 0.
+  unsigned long _avr_timer_M = 1;	       // Start count from here, down to 0. Default 1 ms.
+  unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
+  unsigned short count = 0x00;
+  void TimerOn() {
+	  // AVR timer/counter controller register TCCR1
+	  TCCR1B = 0x0B;     // bit3 = 0: CTC mode (clear timer on compare)
+	  // bit2bit1bit0 = 011: pre-scaler /64
+	  // 00001011: 0x0B
+	  // SO, 8 MHz clock or 8,000,000 / 64 = 125,000 ticks/s
+	  // Thus, TCNT1 register will count at 125,000 tick/s
+	  // AVR output compare register OCR1A.
+	  OCR1A = 125;    // Timer interrupt will be generated when TCNT1==OCR1A
+	  // We want a 1 ms tick. 0.001s * 125,000 ticks/s = 125
+	  // So when TCNT1 register equals 125,
+	  // 1 ms has passed. Thus, we compare to 125.
+	  // AVR timer interrupt mask register
+	  TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+	  // Initialize avr counter
+	  TCNT1 = 0;
+	  _avr_timer_cntcurr = _avr_timer_M;
+	  // TimerISR will be called every _avr_timer_cntcurr milliseconds
+	  // Enable globla interrupts
+	  SREG |= 0x80; // 0x80: 1000000
+  }
+  void TimerOff() {
+	  TCCR1B = 0x00; /// bit3bit1bit0 = 000: timer off
+  }
+  void TimerISR() {
+	  TimerFlag = 1;
+  }
+  // In our approach, the C programmer does not touch this ISR, but rather TimerISR()
+  ISR(TIMER1_COMPA_vect) {
+	  // CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+	  _avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
+	  if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
+		  TimerISR(); 	      // Call the ISR that the user uses
+		  _avr_timer_cntcurr = _avr_timer_M;
+	  }
+  }
+  // Set TimerISR() to tick every M ms
+  void TimerSet(unsigned long M) {
+	  _avr_timer_M = M;
+	  _avr_timer_cntcurr = _avr_timer_M;
+  }
+
+
+
+void InitADC(void)
+{
+	ADMUX|=(1<<REFS0);
+	ADCSRA|=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); //ENABLE ADC, PRESCALER 128
+}
+
+uint16_t readadc(uint8_t ch)
+{
+	ch&=0b00000111;         //ANDing to limit input to 7
+	ADMUX = (ADMUX & 0xf8)|ch;  //Clear last 3 bits of ADMUX, OR with ch
+	ADCSRA|=(1<<ADSC);        //START CONVERSION
+	while((ADCSRA)&(1<<ADSC));    //WAIT UNTIL CONVERSION IS COMPLETE
+	return(ADC);        //RETURN ADC VALUE
 }
 
 
 
-//NOTES TO SELF
-/* -Make sure to implement a getPos function for all states to create bit strings for each object
+
+
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////-----------GLOBAL-----------///////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+//FUNCS
+void updateArrays();
+void updateBullets();
+void updatePlayer();
+
+//VARS
+char rowArray[6] = {0}; // The 6th element of col/rows is reserved for the player. 
+char colArray[6] = {0};
+int score = 0;
+uint16_t x, y;
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////-----------BULLET-----------///////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+struct bullet {
+	char x; //X & Y start from 0 for array access
+	char y;
+	char startX;
+	char startY;
+	char direction; // 0 up, 1 down, 2 left, 3 right
+
+};
+
+struct bullet bulletArray[5]; 
+
+
+enum Bullet_States {bullet_init, update} bullet_state;
+
+int bulletCounter = 0;
+void bulletTick() {
+		switch(bullet_state) {
+			case bullet_init:
+				bullet_state = update;
+				break;
+			case update:
+				if (bulletCounter == 1000) {
+					bulletCounter = 0;
+				updateArrays();
+				updateBullets();
+				}
+				bulletCounter++;
+				break;
+			
+			
+		}
+		
+		switch(bullet_state) {
+			case bullet_init:
+				break;
+			case update:
+				break;
+			
+		}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////-----------PLAYER-----------///////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+struct bullet Player = {4, 4, 0, 0, 0};
+
+enum Player_States {player_init, player_idle, player_move, player_wait} player_state;
+enum Player_Directions {Up, Down, Left, Right, init} player_direction;
+int player_count = 0;
+
+void playerTick() {
+	switch(player_state) {
+		case player_init: 
+				player_direction = init;
+				player_state = player_idle;
+			break;
+		case player_idle:
+			if (x > 600) {
+				player_direction = Right;
+				player_state = player_move;
+			}
+			else if (x < 400) {
+				player_direction = Left;
+				player_state = player_move;
+			}
+			else if (y > 600) {
+				player_direction = Up;
+				player_state = player_move;
+			}
+			else if (y < 400) {
+				player_direction = Down;
+				player_state = player_move;
+				
+			}
+			updatePlayer();
+
+			break;
+		case player_move:
+			if (player_direction == Up) {
+				if (Player.y < 7) {
+					Player.y += 1;
+				}
+			}
+			else if (player_direction == Down) {
+				if (Player.y > 0) {
+					Player.y -= 1;
+					
+				}
+			}
+			else if (player_direction == Left) {
+				if (Player.x > 0) {
+					Player.x -= 1;
+				}
+			}
+			else if (Player.direction == Right) {
+				if (Player.x < 7) {
+					Player.x += 1;
+				}
+				
+			}
+			player_state = player_wait;
+		
+			break;
+		case player_wait:
+			player_count++;
+					if (player_count == 100) {
+						player_direction = init;
+						player_state = player_idle;
+						player_count = 0;
+						
+						}
+			break;
+	}
+	
+	
+	
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////-----------RENDER-----------///////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+enum Render_States {render_init, draw} render_state;
+
+int i = 0;
+
+void renderTick() {
+	switch(render_state) {
+		case render_init:
+			render_state = draw;
+			break;
+		case draw: //Draw the board by getting data from bullets & player
+				
+				PORTB = rowArray[i];
+				PORTD = ~colArray[i];
+				i++;
+				if (i == 6) {
+					i = 0;
+				}
+			break;
+			
+	}
+	
+	
+	
+}
+
+int main(void) {
+	DDRD = 0xFF; PORTD = 0x00; // MATRIX ROWS
+	DDRB = 0xFF; PORTB = 0x00; // MATRIX COLUMNS
+	DDRA = 0x00; PORTA = 0xFF; // JOYSTICK INPUT
+	TimerOn();
+	TimerSet(1); 
+	InitADC();
+	bullet_state = bullet_init;
+	render_state = render_init;
+	player_state = player_init;
+	
+	while(1) {
+		x = readadc(1); //For some reason this is backwards, so 1 and 0 are switched
+		y = readadc(0);
+		bulletTick();
+		renderTick();
+		playerTick();
+	
+		while(!TimerFlag);
+		TimerFlag = 0;
+		
+	}
+	
+
+	
+	
+	
+	return 1;
+}
+
+
+void updateBullets() {
+	int b = 0;
+	int bDirection;
+	for (b = 0; b < 5; b++) {
+		bDirection = bulletArray[b].direction;
+		
+		if (bDirection == 0) {
+			bulletArray[b].y += 1;
+			
+		}
+		else if (bDirection == 1) {
+			bulletArray[b].y -= 1;
+		}
+		else if (bDirection == 2) {
+			bulletArray[b].x -=1;
+		} 
+		else if (bDirection == 3) {
+			bulletArray[b].x +=1;
+		}
+		
+		if ((bulletArray[b].x > 8) || (bulletArray[b].y > 8) || (bulletArray[b].x < 0) || (bulletArray[b].y < 0)) { //Respawn bullet
+			
+			score++;
+
+			bulletArray[b].direction = rand() % 4; 
+			if (bulletArray[b].direction == 0) { //UP     //Direction checks to make sure new bullets don't continue off screen based on their spawn point
+				bulletArray[b].startX = rand() % 8;
+				bulletArray[b].startY = 0;
+			}
+			else if (bulletArray[b].direction == 1) { //DOWN
+				bulletArray[b].startX = rand() % 8;
+				bulletArray[b].startY = 7;
+			}
+			else if (bulletArray[b].direction == 2) { //LEFT
+				bulletArray[b].startX = 7;
+				bulletArray[b].startY = rand() % 8;
+			}
+			else if (bulletArray[b].direction == 3) { //RIGHT
+				bulletArray[b].startX = 0;
+				bulletArray[b].startY = rand() % 8;
+			}
+			
+						bulletArray[b].x = bulletArray[b].startX;
+						bulletArray[b].y = bulletArray[b].startY;
+		}
+			
+	}
+		
+		
+		
+}
+
+
+void updatePlayer() {
+	
+	rowArray[5] = 0b00000001 << (Player.x);
+	colArray[5] = 0b00000001 << (Player.y);
+	
+}
+	
+
+
+void updateArrays() {
+	int j = 0;
+	for (j = 0; j < 5; j++) { //We only update the first 5 elements of the row/colArrays since element 6 is handled by updatePlayer
+		int posX = bulletArray[j].x;
+		int posY = bulletArray[j].y;
+		
+		rowArray[j] = 0b00000001 << (posX);
+		colArray[j] = 0b00000001 << (posY);
+	
+	}
+	
+	
+}
+   
+   
+   
+   
